109c49f184d1e5c5e0b00bef302b4409c665bd57
Ahsan Ali <436ahsan@gmail.com>
2024-03-05 04:57:21 -0700

    modified AMG solver script with added options

--- Code/air_driver.py
+++ Code/air_driver.py
@@ -2,31 +2,38 @@
 # Import required packages
 import pyamg
 from pyamg.classical import air_solver
+from pyamg.aggregation import energymin_cf_solver
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.io as sio
 
 
-# Parameters
+
+# AMG Parameters
 maximum_levels = 30         # Max levels in hierarchy
 maximum_coarse = 30         # Max points allowed on coarse grid
-tolerance = 1e-10      # Residual convergence tolerance
-coarse_solver = 'pinv'
+tolerance = 1e-9      # Residual convergence tolerance
+coarse_solver = 'splu'
 krylov = 'gmres'
 keep = False
-strength=('classical', {'theta': 0.25, 'norm': 'min'})
+strength=('classical', {'theta': 0.01})
 splitting = ('RS', {'second_pass': True})
-interp ='one_point'
+interp ='classical'
 restriction = ('air', {'theta': 0.05, 'degree': 2})
-prerelax = None
-postrelax = ('fc_jacobi', {'omega': 1.0, 'iterations': 1,'withrho': False, 'f_iterations': 2,'c_iterations': 1})
+prerelax  =('cf_jacobi', {'omega': 1.0, 'iterations': 1, 'withrho': True, 'f_iterations': 2, 'c_iterations': 1})
+postrelax = ('fc_jacobi', {'omega': 1.0, 'iterations': 1,'withrho': True, 'f_iterations': 2,'c_iterations': 1})
+#prepost = ('gauss_seidel_nr', {'sweep': 'symmetric', 'iterations': 1})
 filter_entries = None
-maxiter = 300
+maxiter = 1000
 
 
 
-# 2D Poisson problem on 100x100 grid
-A = pyamg.gallery.poisson((100,100), format='csr')
- 
+# Load the problem matrix and filter the small entries
+data = sio.loadmat('MMCP.mat')
+A=data['A']
+A = A.tocsr()
+A.data[np.abs(A.data) < 1e-16] = 0
+A.eliminate_zeros() 
         
 # Random initial guess and zero right hand side (Solving Ax = 0)
 x0 = np.random.rand(A.shape[0])
@@ -34,9 +41,18 @@ b = np.zeros((A.shape[0],))
 
         
 # Create a multilevel solver using approximate ideal restriction (AIR) AMG
+
 ml = air_solver(A,strength=strength,CF=splitting,interpolation=interp,restrict=restriction,
                 presmoother =prerelax,postsmoother =postrelax,filter_operator = filter_entries,
-                max_levels=maximum_levels,max_coarse=maximum_coarse,keep=keep,coarse_solver=coarse_solver)
+                max_levels=maximum_levels,keep=keep,max_coarse=maximum_coarse,coarse_solver=coarse_solver)
+
+
+# Create a multilevel solver using classical Ruge-Stuben AMG Solver
+
+# ml = pyamg.ruge_stuben_solver(A,strength = strength,CF = splitting,interpolation = interp,
+#                               presmoother = prerelax,postsmoother = postrelax,
+#                               max_levels = maximum_levels,max_coarse = maximum_coarse,
+#                               keep = keep,coarse_solver=coarse_solver)
 
 
 # Solve Ax=b with GMRES (AMG as a preconditioner to GMRES)
@@ -48,11 +64,11 @@ complexity_air = ml.operator_complexity()
 grid_complexity_air=ml.grid_complexity()
 size_air = A.shape[0]
 print(ml)
-
+print(resvec)
         
         
 # Print AMG detailed results
-print("AIR solver results ")
+print("AMG solver results ")
 print("{:^9s} | {:^9s}  | {:^9s} | {:^9s} | {:^9s} | {:^9s}".format(
     "n", "iter", "rho", "OpCx", "GdCx", "Work"))
 print("-----------------------------------------------------------------------")
@@ -61,7 +77,7 @@ print("{:^9d} | {:^9d}  | {:^9.2g} | {:^9.2g} | {:^9.2g} | {:^9.2g}".format(
    size_air, Iterations, factors_air,complexity_air,grid_complexity_air,
     complexity_air / (-np.log10(factors_air))))
 
-plt.semilogy(resvec/resvec[0], 'o-')
+plt.semilogy(resvec/resvec[0], '-')
 plt.xlabel('iterations')
 plt.ylabel('normalized residual')
 plt.show()
